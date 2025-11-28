e0ae8d28892eec509e636f058161c3d9edb61735
Rommel Bandeira <rommelb@gmail.com>
2025-11-27 21:50:30 -0300

    fix: export TenantMembership type to resolve build error

--- src/db/schema/tenants/types.ts
+++ src/db/schema/tenants/types.ts
@@ -1,5 +1,9 @@
+import { tenantMembershipTable } from "./memberships";
 import { tenantTable } from "./tables";
 
 export type Tenant = typeof tenantTable.$inferSelect;
 export type NewTenant = typeof tenantTable.$inferInsert;
+
+export type TenantMembership = typeof tenantMembershipTable.$inferSelect;
+export type NewTenantMembership = typeof tenantMembershipTable.$inferInsert;
 
